a9d66fa602f20a0980c8ff51780a8ba51d45026d
Khadija <km9600455@gmail.com>
2023-06-24 01:57:48 -0700

    Initial Commit

---
+++ counter2/counter2App.swift
@@ -0,0 +1,17 @@
+//
+//  counter2App.swift
+//  counter2
+//
+//  Created by Khadija on 23/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct counter2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
